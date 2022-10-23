da5fed6986a16586e345ee7068306830b01e95c0
AKASH <ag565609@gmail.com>
2022-10-23 16:06:14 +0530

    1.1.5

--- index.js
+++ index.js
@@ -1387,13 +1387,12 @@ app.post("/reset-password-submit", async (req, res) => {
 //Reset password ends
 
 //heroku
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
-    })
+if (process.env.NODE_ENV) {
+    app.use(express.static('client/build'));
+    app.get("*", function (req, res) {
+        res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    });
 }
-
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
