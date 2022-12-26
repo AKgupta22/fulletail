eec0d1c98140b7799196a9e538e1e9c08ea114b5
saurabh <saurabh@subcodevs.com>
2022-12-26 17:57:07 +0530

    fixed not found

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ require('./Routes/Google_Verify')(app)
 if (process.env.NODE_ENV) {
     app.use(express.static('client/build'));
     app.get("*", function (req, res) {
-        res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+        res.sendFile(path.resolve(__dirname, './client/build/index.html'))
     });
 }
 const PORT = process.env.PORT || 8000
